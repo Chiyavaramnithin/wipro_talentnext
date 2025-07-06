646e1de62c0920984bed355fb49497776b269135
nithin <nithinchiyavaram@gmail.com>
2025-07-06 20:39:51 +0530

    java_fundamental

--- java_fundamental/src/java_fundamental/Question18.java
+++ java_fundamental/src/java_fundamental/Question18.java
@@ -1,9 +1,28 @@
 package java_fundamental;
+import java.util.Scanner;
 
 public class Question18 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+				System.out.print("Enter a number: ");
+				int number = sc.nextInt();
+				
+				int reversed = 0;
+				int n = Math.abs(number);
+				
+				while (n != 0) {
+					int digit = n % 10;
+					reversed = reversed * 10 + digit;
+					n /= 10;
+				}
+				
+				if (number < 0) {
+					reversed = -reversed;
+				}
+				
+				System.out.println("Reversed number: " + reversed);
 
 	}
 
---
+++ java_fundamental/src/java_fundamental/Question19.java
@@ -0,0 +1,29 @@
+package java_fundamental;
+import java.util.Scanner;
+
+public class Question19 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+				System.out.println("Enter a digit: ");
+				int n = sc.nextInt();
+				int rev = 0;
+				int temp = n;
+				while (temp != 0) {
+					int digit = temp % 10;
+					rev = rev * 10 + digit;
+					temp = temp / 10;
+
+				}
+				if(rev==n) {
+					System.out.println(n + " is a Palindrome");
+				}
+				else {
+					System.out.println(n + " is not a Palindrome");
+				}
+
+		}
+
+	}
+
