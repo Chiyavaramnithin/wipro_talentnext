b2929b726a8aaddd37d633fbb03702961afffc77
nithin <nithinchiyavaram@gmail.com>
2025-07-07 10:57:40 +0530

    java_fundamental

---
+++ java_fundamental/src/java_fundamental/Question24.java
@@ -0,0 +1,14 @@
+package java_fundamental;
+import java.util.Scanner;
+
+public class Question24 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		     int[]a= {65,68,69};
+		     for(int i:a) {
+		    	 System.out.print((char)i+" ");
+		     }
+			}
+
+		}
---
+++ java_fundamental/src/java_fundamental/Question34.java
@@ -0,0 +1,34 @@
+package java_fundamental;
+
+public class Question34 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  if (args.length != 9) {
+	            System.out.println("Please enter 9 integer numbers");
+	            return;
+	        }
+
+	        int[][] array = new int[3][3];
+	        int index = 0;
+	        int max = Integer.MIN_VALUE;
+
+	        System.out.println("The given array is :");
+
+	        for (int i = 0; i < 3; i++) {
+	            for (int j = 0; j < 3; j++) {
+	                array[i][j] = Integer.parseInt(args[index]);
+	                System.out.print(array[i][j] + " ");
+	                if (array[i][j] > max) {
+	                    max = array[i][j];
+	                }
+	                index++;
+	            }
+	            System.out.println();
+	        }
+
+	        System.out.println("The biggest number in the given array is " + max);
+
+	}
+
+}
